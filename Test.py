01f3a583b8e023a1def2910f5a386a3f8f3acc9c
Himaruth <himaruths@gmail.com>
2023-08-31 20:52:07 +0530

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+def find_largest(num1, num2, num3):
+  """Finds the largest number among the three given numbers."""
+  if num1 >= num2 and num1 >= num3:
+    return num1
+  elif num2 >= num1 and num2 >= num3:
+    return num2
+  else:
+    return num3
+
+st.title("Find the Largest Number")
+
+num1 = st.number_input("Enter the first number:")
+num2 = st.number_input("Enter the second number:")
+num3 = st.number_input("Enter the third number:")
+
+largest_number = find_largest(num1, num2, num3)
+
+st.write("The largest number is:", largest_number)
